7598bd3242e647c3179a78d2f1ca06df007073d8
Michael Romashov <mromashov@icloud.com>
2022-11-17 00:33:15 -0500

    chore: add eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,12 @@
  * SPDX-License-Identifier: MIT
  */
 
-// TODO: Unified .eslintrc.js for all Synaptic projects
 module.exports = {
     root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
     extends: [
-        '@flybywiresim/eslint-config',
+        '@synaptic-simulations/eslint-config',
     ],
-    rules: {
-        'import/no-unresolved': 'off',
-        'no-undef': 'off',
-        'no-nested-ternary': 'off',
-        'linebreak-style': 'off',
-        'no-underscore-dangle': 'off',
-        'react/jsx-one-expression-per-line': 'off',
-        '@typescript-eslint/consistent-type-definitions': 'error',
-        '@typescript-eslint/member-delimiter-style': ['error', { singleline: { delimiter: 'comma' } }],
-        'indent': ['error', 4, { SwitchCase: 1 }],
-        'default-case': 'off',
-    },
+    ignorePatterns: [
+        'src/templates/**/*',
+    ],
 };
